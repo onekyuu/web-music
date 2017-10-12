ca6f471559f9a73c3459cef1597cc41335d37b08
wky <wky_0615@163.com>
2017-10-12 15:38:34 +0800

    commit

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
 
-var xhr = new XMLHttpRequest()
-xhr.open('GET', '/music.json', true)
-xhr.onload = function(){
-  if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
-    console.log(this.responseText)
-  }else{
-    console.log('获取数据失败')
+getMusicList(function(list){
+  console.log(list)
+})
+
+function getMusicList(callback){
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', '/music.json', true)
+  xhr.onload = function(){
+    if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
+      callback(JSON.parse(this.responseText))
+    }else{
+      console.log('获取数据失败')
+    }
   }
+  xhr.send()
 }
-xhr.send()
