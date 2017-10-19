532cffde6a98d00139c56d177eedf90f949d4395
wky <wky_0615@163.com>
2017-10-19 21:58:33 +0800

    commit

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 
 
+var musicList = []
+var currentIndex = 0
+
+function $(selector){
+  return document.querySelector(selector)
+}
+function $$(selector){
+  return document.querySelectorAll(selector)
+}
+
 getMusicList(function(list){
-  console.log(list)
+  musicList = list
+  console.log(musicList)
+  setChannels(list)
 })
 
 function getMusicList(callback){
   var xhr = new XMLHttpRequest()
-  xhr.open('GET', '/music.json', true)
+  xhr.open('GET', 'https://jirenguapi.applinzi.com/fm/getChannels.php', true)
   xhr.onload = function(){
     if((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304){
       callback(JSON.parse(this.responseText))
@@ -15,4 +27,12 @@ function getMusicList(callback){
     }
   }
   xhr.send()
+}
+
+function setChannels(musicObj){
+  console.log(musicObj)
+  for(var i=0; i<$$('.category p').length; i++){
+    $$('.category p')[i].innerText = musicObj.channels[i].name
+    $$('.category p')[i].className = musicObj.channels[i].channel_id
+  }
 }
