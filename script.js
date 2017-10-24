c4298b9b8c6bbc0799477cf3158052e7fb1e661d
wky <wky_0615@163.com>
2017-10-24 16:02:54 +0800

    commit

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
 var album = {
   init: function(){
+    this.$ul = $('.album ul')
+    this.getData()
+  },
+  start: function(){
+
+  },
+  event: function(){
+
+  },
+  getData: function(){
+    var me = this
+    $.ajax({
+      url: 'https://jirenguapi.applinzi.com/fm/getChannels.php',
+      type: 'GET',
+      dataType: 'jsonp'
+    }).done(function(ret){
+      console.log(ret)
+      me.createNode(ret)
+    }).fail(function(){
+      console.log('error')
+    })
+  },
+  createNode: function(data){
+    var me = this
+    var template = ''
+    data.channels.forEach(function(album){
+      template += '<li class="category" data_name="'+album.channel_id+'">'+
+                    '<div class="pic" style="background-image:url('+album.cover_small+')">'+
+                    '</div>'+
+                    '<p class="title">'+album.name+'</p>'+
+                  '</li>'
+    })
+    me.$ul.html(template)
+    this.render()
+  },
+  render: function(){
+    var count = this.$ul.find('li').length
+    var width = this.$ul.find('li').outerWidth(true)
+    this.$ul.css({
+      width: count * width + 'px'
+    })
+  }
+}
+
+
+var player = {
+  init: function(){
 
   },
   start: function(){
@@ -9,14 +56,13 @@ var album = {
 
   },
   getData: function(){
-    $ajax({
-      
-    })
+
   },
-  render: function(data){
+  render: function(){
 
   }
 }
 
 
 album.init()
+player.init()
