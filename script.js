1b465435896c5167f2624745fb61383da0f143da
wky <wky_0615@163.com>
2017-10-25 22:30:46 +0800

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//专辑模块
 var album = {
   init: function(){
     this.$album = $('.album')
@@ -10,23 +11,55 @@ var album = {
     this.getData()
     this.event()
   },
-  start: function(){
-
-  },
   event: function(){
+    //滚动专辑列表
     var me = this
     me.$right.on('click', function(){
+      //判断动画是否执行完毕
+      if(me.isAnimateFinish) return
       var liWidth = me.$ul.find('li').outerWidth(true)
       var count = Math.floor(me.$box.width()/liWidth)
+      //判断是否滚动到最后
       if(!me.isToEnd){
+        me.isAnimateFinish = true
         me.$ul.animate({
           left: '-='+liWidth*count
-        }, 500, function(){
-          if(me.$box.width() - parseInt(me.$ul.css('left')) >= me.$ul.width()){
+        }, 1000, function(){
+          me.isToStart = false
+          me.isAnimateFinish = false
+          if(me.$box.width() - parseFloat(me.$ul.css('left')) >= me.$ul.width()){
             me.isToEnd = true
           }
         })
       }
+    })
+    me.$left.on('click', function(){
+      if(me.isAnimateFinish) return
+      var liWidth = me.$ul.find('li').outerWidth(true)
+      var count = Math.floor(me.$box.width()/liWidth)
+      if(!me.isToStart){
+        me.isAnimateFinish = true
+        me.$ul.animate({
+          left: '+='+liWidth*count
+        }, 1000, function(){
+          me.isAnimateFinish = false
+          me.isToEnd = false
+          if(parseFloat(me.$ul.css('left')) >= 0){
+            me.isToStart = true
+          }
+        })
+      }
+    })
+
+    //添加专辑选中状态
+    me.$ul.on('click', 'li', function(){
+      $(this).addClass('active').siblings().removeClass('active')
+    })
+
+    //使用自定义事件传递专辑信息
+    eventCenter.fire('select', {
+      channel_id : $(this).attr('data_id'),
+      channelName : $(this).attr('data_name')
     })
   },
   getData: function(){
@@ -46,16 +79,17 @@ var album = {
     var me = this
     var template = ''
     data.channels.forEach(function(album){
-      template += '<li class="category" data_name="'+album.channel_id+'">'+
+      template += '<li class="category" data_id="'+album.channel_id+' data_name'+album.name+'">'+
                     '<div class="pic" style="background-image:url('+album.cover_small+')">'+
                     '</div>'+
                     '<p class="title">'+album.name+'</p>'+
                   '</li>'
     })
     me.$ul.html(template)
-    this.render()
+    this.setWidth()
   },
-  render: function(){
+  //设置专辑列表长度
+  setWidth: function(){
     var count = this.$ul.find('li').length
     var width = this.$ul.find('li').outerWidth(true)
     this.$ul.css({
@@ -64,7 +98,7 @@ var album = {
   }
 }
 
-
+//播放器模块
 var player = {
   init: function(){
 
@@ -76,10 +110,29 @@ var player = {
 
   },
   getData: function(){
-
+    var me = this
+    $.ajax({
+      url: 'https://jirenguapi.applinzi.com/fm/getSong.php',
+      type: 'GET',
+      dataType: 'jsonp'
+    }).done(function(ret){
+      console.log(ret)
+    }).fail(function(){
+      console.log('error')
+    })
   },
   render: function(){
 
+  }
+}
+
+
+var eventCenter = {
+  on: function(type, handler){
+    $(document).on(type, handler)
+  },
+  fire: function(type, data){
+    $(document).trigger(type, data)
   }
 }
 
